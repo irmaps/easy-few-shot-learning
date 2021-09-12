80709df21abd07515600f93e62d8d9f148b59146
Irma Permata Sari <70065979+irmaps@users.noreply.github.com>
2021-09-13 04:14:57 +0700

    Update __init__.py

--- easyfsl/methods/__init__.py
+++ easyfsl/methods/__init__.py
@@ -2,3 +2,4 @@ from .abstract_meta_learner import AbstractMetaLearner
 from .matching_networks import MatchingNetworks
 from .prototypical_networks import PrototypicalNetworks
 from .relation_networks import RelationNetworks
+from .relation_networks import MAML
